8ed3b012fa288399fb9b41f345d6f0ec2305f9ae
Juan <61684795+juanjq@users.noreply.github.com>
2021-08-17 14:19:29 +0200

    Create plt_config.py

---
+++ plt_config.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+from functools import partial
+
+def params():   
+    #fontsize
+    plt.rcParams['font.size'] = 18   
+    #figure size
+    plt.rcParams['figure.figsize'] = 16,9    
+    #LaTex font
+    plt.rcParams['mathtext.fontset'] = 'custom'
+    plt.rcParams['mathtext.rm'] = 'Bitstream Vera Sans'
+    plt.rcParams['mathtext.it'] = 'Bitstream Vera Sans:italic'
+    plt.rcParams['mathtext.bf'] = 'Bitstream Vera Sans:bold'
+    plt.rcParams['mathtext.fontset'] = 'stix'
+    plt.rcParams['font.family'] = 'STIXGeneral'
+    #ticks
+    plt.rcParams["xtick.direction"] = "in"
+    plt.rcParams["ytick.direction"] = "in"
+    plt.rcParams['xtick.top'] = True
+    plt.rcParams['ytick.right'] = True
+    plt.rcParams['xtick.minor.visible'] = True
+    plt.rcParams['ytick.minor.visible'] = True
+    plt.rcParams['xtick.major.size'] = 8
+    plt.rcParams['xtick.major.width'] = 2
+    plt.rcParams['xtick.minor.size'] = 5
+    plt.rcParams['xtick.minor.width'] = 1
+    plt.rcParams['ytick.major.size'] = 8
+    plt.rcParams['ytick.major.width'] = 2
+    plt.rcParams['ytick.minor.size'] = 5
+    plt.rcParams['ytick.minor.width'] = 1
+    #figure
+    plt.rcParams['figure.facecolor'] = 'w'
+    plt.rcParams['figure.frameon'] = False
+    plt.rcParams['figure.dpi'] = 80
+    plt.rcParams['savefig.dpi'] = 200
+    plt.rcParams['savefig.format'] = 'pdf'
+    plt.rcParams['savefig.transparent'] = True
+    plt.rcParams['figure.dpi'] = 100
+    #errorbars
+    plt.rcParams['errorbar.capsize'] = 3
+    #histograms
+    plt.rcParams['hist.bins'] = 30
+    #lines
+    plt.rcParams['lines.linewidth'] = 1
+    plt.rcParams['lines.marker'] = None
+    #axes
+    plt.rcParams['axes.facecolor'] ='w'
+    plt.rcParams['axes.edgecolor'] ='k'
+    plt.rcParams['axes.linewidth'] =1
+    plt.rcParams['axes.titlesize'] = 18
+    plt.rcParams['axes.labelsize'] = 18
+    plt.rcParams['axes.labelcolor'] = 'k'
+    #grid
+    plt.rcParams['axes.grid'] =False
+    plt.rcParams['grid.color'] ='k'
+    plt.rcParams['grid.linestyle'] ='-'
+    plt.rcParams['grid.linewidth'] =0.5
+    plt.rcParams['grid.alpha'] =1
+    #legend
+    plt.rcParams['legend.fancybox'] =True
+    plt.rcParams['legend.fontsize'] =17
+    plt.rcParams['legend.markerscale'] =1.5
+    #colors
+    plt.rcParams['axes.prop_cycle'] = plt.cycler(color=[
+        'darkblue','darkviolet','deeppink','crimson','darkorange','gold'])
+    
+def default():
+    plt.rcParams.update(plt.rcParamsDefault)
+
+context = partial(plt.rc_context, rc={'figure.figsize':(10,9),
+                                            'font.size':17,
+                                            'mathtext.fontset':'custom',
+                                            'mathtext.rm':'Bitstream Vera Sans',
+                                            'mathtext.it':'Bitstream Vera Sans:italic',
+                                            'mathtext.bf':'Bitstream Vera Sans:bold',
+                                            'mathtext.fontset':'stix',
+                                            'font.family':'STIXGeneral',
+                                            'xtick.direction':'in',
+                                            'ytick.direction':'in',
+                                            'xtick.major.size':8,
+                                            'xtick.major.width':2,
+                                            'xtick.minor.size':5,
+                                            'xtick.minor.width':1,
+                                            'ytick.major.size':8,
+                                            'ytick.major.width':2,
+                                            'ytick.minor.size':5,
+                                            'ytick.minor.width':1,
+                                            'xtick.top':True,
+                                            'ytick.right':True,
+                                            'xtick.minor.visible':True,
+                                            'ytick.minor.visible':True })
